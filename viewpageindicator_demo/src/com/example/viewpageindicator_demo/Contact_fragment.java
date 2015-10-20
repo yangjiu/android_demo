bb32dc2f60b0ee9fd599a512dea82d73d80ea837
liwenfeng <wenfeng.li@oppo.com>
2015-10-20 12:00:12 +0800

    viewpageindicator create

---
+++ viewpageindicator_demo/src/com/example/viewpageindicator_demo/Contact_fragment.java
@@ -0,0 +1,29 @@
+package com.example.viewpageindicator_demo;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Contact_fragment extends Fragment {
+         
+	@Override
+	public void onCreate( Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	
+	public View onCreateView(LayoutInflater inflater,
+			 ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.contact_fragment, null);
+	}
+	
+	
+}
---
+++ viewpageindicator_demo/src/com/example/viewpageindicator_demo/Wechat_fragment.java
@@ -0,0 +1,26 @@
+package com.example.viewpageindicator_demo;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Wechat_fragment extends Fragment {
+	@Override
+	public void onCreate( Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	
+	public View onCreateView(LayoutInflater inflater,
+			 ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.wechat_fragment, null);
+	}
+	
+}
